835c5a8443182972be37e6214120a15b2e1c5ea9
xp664221812 <realxp141414@163.com>
2020-05-19 18:11:38 +0800

    add

--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertrip/model/common_model.dart';
 import 'package:fluttertrip/model/grid_nav_model.dart';
 
 class GridNav extends StatelessWidget {
@@ -8,7 +12,41 @@ class GridNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[],
+    );
+  }
+
+  _gridItems(BuildContext context) {
+    List<Widget> items = [];
+    if (gridNavModel == null) {
+      return items;
+    }
+
+    if (gridNavModel.hotel != null) {}
+    if (gridNavModel.flight != null) {}
+    if (gridNavModel.travel != null) {}
+  }
+
+  _gridNavItem(BuildContext context, GridNavItem item, bool isFirst) {}
+
+  _mainItem(BuildContext context, CommonModel model) {
+    return GestureDetector(
+        onTap: () {
+
+        },
+        child: Stack(
+          children: <Widget>[
+            Image.network(model.icon,
+              height: 88, width: 121,
+              alignment: AlignmentDirectional.bottomEnd,
+            ),
+            Text(model.title, style: TextStyle(
+              fontSize: 14,
+              color: Colors.white
+            ),)
+          ],
+        )
+    )
   }
 }
-
