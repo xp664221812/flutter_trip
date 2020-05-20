a834c11025975cb44226a2539da6295b427d5fff
xp664221812 <realxp141414@163.com>
2020-05-20 18:02:37 +0800

    add

--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -1,9 +1,9 @@
-import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:fluttertrip/model/common_model.dart';
 import 'package:fluttertrip/model/grid_nav_model.dart';
+import 'package:fluttertrip/widget/webview.dart';
 
 class GridNav extends StatelessWidget {
   final GridNavModel gridNavModel;
@@ -28,25 +28,66 @@ class GridNav extends StatelessWidget {
     if (gridNavModel.travel != null) {}
   }
 
-  _gridNavItem(BuildContext context, GridNavItem item, bool isFirst) {}
+  _gridNavItem(BuildContext context, GridNavItem item, bool isFirst) {
+    List<Widget> items = [];
+    items.add(_mainItem(context, item.mainItem))
+  }
 
   _mainItem(BuildContext context, CommonModel model) {
     return GestureDetector(
         onTap: () {
-
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      WebView(
+                        title: model.title,
+                        url: model.url,
+                        statusBarColor: model.statusBarColor,
+                        hideAppBar: model.hideAppBar,
+                      )));
         },
         child: Stack(
           children: <Widget>[
-            Image.network(model.icon,
-              height: 88, width: 121,
+            Image.network(
+              model.icon,
+              height: 88,
+              width: 121,
               alignment: AlignmentDirectional.bottomEnd,
             ),
-            Text(model.title, style: TextStyle(
-              fontSize: 14,
-              color: Colors.white
-            ),)
+            Text(
+              model.title,
+              style: TextStyle(fontSize: 14, color: Colors.white),
+            )
           ],
-        )
-    )
+        ));
   }
+
+  _doubleItem(BuildContext context, CommonModel topModel,
+      CommonModel bottomModel, bool isCenter) {
+    return Column(
+      children: <Widget>[
+        Expanded(child: _item)
+      ],
+    );
+  }
+
+  _item(BuildContext context, CommonModel model, bool isFirst, bool isCenter) {
+    BorderSide borderSide = BorderSide(width: 0.8, color: Colors.white);
+
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(
+                left: borderSide,
+                bottom: isFirst ? borderSide : BorderSide.none
+            )
+        ),
+        child: Center(child: Text(model.title, textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 14))),
+      ),
+    );
+  }
+
 }
